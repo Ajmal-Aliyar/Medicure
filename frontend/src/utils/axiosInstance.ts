77c0ece7332d8b8eaba51740c78c7c920a11f571
AJMALAJU3 <ajmalaju3@users.noreply.github.com>
2025-04-11 11:38:10 +0530

    feat: add axios intereceptor

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -2,10 +2,41 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000', 
+  baseURL: 'http://localhost:3000',
   headers: { 'Content-Type': 'application/json' },
-  withCredentials: true 
+  withCredentials: true, 
 });
 
+export function getAccessTokenFromCookie(): string | null {
+  const match = document.cookie.match(/(^|;\s*)accessToken=([^;]*)/);
+  return match ? decodeURIComponent(match[2]) : null;
+}
 
+api.interceptors.request.use(
+  (config) => {
+    const token = getAccessTokenFromCookie();
 
+    if (!config.headers) {
+      config.headers = {};
+    }
+
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    if (error.response?.status === 401) {
+      console.log('caught error');
+      window.location.href='/auth'
+    }
+
+    return Promise.reject(error);
+  }
+);
