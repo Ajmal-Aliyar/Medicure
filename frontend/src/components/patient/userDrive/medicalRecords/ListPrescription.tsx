2db61b48ddbf775fbbcc42fee195d755eabf1c68
AJMALAJU3 <ajmalaju3@users.noreply.github.com>
2025-05-03 14:06:34 +0530

    feat: add prescription download

--- frontend/src/components/patient/userDrive/medicalRecords/ListPrescription.tsx
+++ frontend/src/components/patient/userDrive/medicalRecords/ListPrescription.tsx
@@ -1,7 +1,8 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { fetchUserPrescriptionsApi, IPrescription } from "../../../../sevices/medicalRecords/medicalRecord";
 import { ChevronLeft, ChevronRight, X } from "lucide-react";
-import { convertTo12HourFormat } from "../../../../utils/timeStructure";
+import { convertToDateAndTime } from "../../../../utils/timeStructure";
+import html2pdf from "html2pdf.js";
 
 
 const ListPrescription: FC = () => {
@@ -12,6 +13,8 @@ const ListPrescription: FC = () => {
     const [limit] = useState<number>(5);
     const [total, setTotal] = useState<number>(0);
     const [selected, setSelected] = useState<IPrescription | null>(null)
+    const pdfRef = useRef<HTMLDivElement>(null);
+    const [download, setDownload] = useState<boolean>(false)
 
     useEffect(() => {
         const fetchTestReports = async () => {
@@ -28,6 +31,19 @@ const ListPrescription: FC = () => {
         };
         fetchTestReports();
     }, [skip]);
+
+
+    const handleDownload = () => {
+        setDownload(true)
+        if (pdfRef.current) {
+            setTimeout(() => {
+                const filename = `${selected?.doctorDetails?.fullName?.replace(/\s+/g, '-')}-prescription.pdf`;
+                html2pdf().from(pdfRef.current).save(filename);
+                setDownload(false)
+            }, 200);
+        }
+    };
+
 
     const handleNext = () => {
         if (skip + limit < total) {
@@ -61,11 +77,11 @@ const ListPrescription: FC = () => {
                         </div>
                         <div className="flex flex-col gap-1 text-sm items-center">
                             <p>diagnosis : {prescription.diagnosis}</p>
-                            <p>date : {convertTo12HourFormat(prescription.createdAt)}</p>
+                            <p>date : {convertToDateAndTime(prescription.createdAt)}</p>
                         </div>
                         <div className="flex gap-4 items-center">
                             <p className="text-blue-500 cursor-pointer mr-3 hover:underline active:scale-95 duration-300"
-                            onClick={() => setSelected(prescription)}>view</p>
+                                onClick={() => setSelected(prescription)}>view</p>
                         </div>
                     </div>
                 )
@@ -86,11 +102,11 @@ const ListPrescription: FC = () => {
                 <button onClick={handleNext} disabled={skip + limit >= total} className="px-2 py-1 bg-[#51aff666] rounded"><ChevronRight /></button>
             </div>
 
-            { selected &&
+            {selected &&
                 <div className="w-[100%] h-[100%] fixed top-0 left-0 bg-black/20 flex justify-center items-center">
                     <div className="min-w-[350px] md:min-w-[450px] relative max-w-[500px] bg-white rounded-md p-2">
-                    <div className="absolute right-3 top-3 text-white bg-[#51aff6c5] rounded-sm active:scale-95"
-                    onClick={() => setSelected(null)}><X /> </div>
+                        <div className="absolute right-3 top-3 text-white bg-[#51aff6c5] rounded-sm active:scale-95"
+                            onClick={() => setSelected(null)}><X /> </div>
                         <div className="flex gap-4 items-center border-b pb-2 border-gray-200">
                             <img src={selected?.doctorDetails.profileImage} className="w-16 rounded-full overflow-hidden" alt="" />
                             <div>
@@ -98,16 +114,48 @@ const ListPrescription: FC = () => {
                                 <p className="text-md text-[#51aff6] font-medium ">{selected?.doctorDetails.specialization}</p>
                             </div>
                         </div>
-                            <div className="grid grid-cols-3 text-sm text-gray-500 gap-2 py-3">
-                                <p>Diagnosis</p>
-                                <p className="col-span-2">{selected.diagnosis}</p>
-                                <p>Allergy</p>
-                                <p className="col-span-2">{selected.allergy}</p>
-                                <p>Prescription</p>
-                                <p className="col-span-2">{selected.prescription}</p>
-                                <p>Date</p>
-                                <p className="col-span-2">{convertTo12HourFormat(selected.createdAt)}</p>
-                            </div>
+                        <div
+                            ref={pdfRef}
+                            style={{
+                                fontFamily: "Arial, sans-serif",
+                                fontSize: "14px",
+                                color: "#333",
+                                padding: "20px",
+                                borderRadius: "8px",
+                                maxWidth: "100%",
+                                backgroundColor: "#fff"
+                            }}
+                        >
+                            <h2 style={{ textAlign: "center", marginBottom: "20px" }}>Prescription Summary</h2>
+
+                            <table style={{ width: "100%", borderCollapse: "collapse" }}>
+                                <tbody>
+                                    <tr>
+                                        <td style={{ padding: "8px", border: "1px solid #ccc", fontWeight: "bold" }}>Diagnosis</td>
+                                        <td style={{ padding: "8px", border: "1px solid #ccc" }}>{selected.diagnosis}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{ padding: "8px", border: "1px solid #ccc", fontWeight: "bold" }}>Allergy</td>
+                                        <td style={{ padding: "8px", border: "1px solid #ccc" }}>{selected.allergy}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{ padding: "8px", border: "1px solid #ccc", fontWeight: "bold" }}>Prescription</td>
+                                        <td style={{ padding: "8px", border: "1px solid #ccc" }}>{selected.prescription}</td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{ padding: "8px", border: "1px solid #ccc", fontWeight: "bold" }}>Date</td>
+                                        <td style={{ padding: "8px", border: "1px solid #ccc" }}>{convertToDateAndTime(selected.createdAt)}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div className="w-full flex justify-end">
+                            <button
+                            disabled={download}
+                                onClick={handleDownload}
+                             className={`px-2 border rounded-md mt-2 mr-2 active:scale-95 cursor-pointer ${download ? 'border-gray-400 text-gray-400' : ''}`}>Download</button>
+                        </div>
                     </div>
                 </div>
             }
