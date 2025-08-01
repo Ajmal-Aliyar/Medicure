e7db63f51f05a2b064390585b378b42f4b938a53
AJMALAJU3 <ajmalaju3@users.noreply.github.com>
2025-08-01 09:22:59 +0530

    replace redis local with cloud

--- backend-solid/src/infrastructure/cache/redis-service.ts
+++ backend-solid/src/infrastructure/cache/redis-service.ts
@@ -1,16 +1,20 @@
-import { RedisClientType } from "redis";
 import { inject, injectable } from "inversify";
-import {TYPES} from "@/di/types";
+import { TYPES } from "@/di/types";
 import { ICacheService } from "@/interfaces";
+import Redis from "ioredis"; 
 
 @injectable()
 export class RedisService implements ICacheService {
   constructor(
-    @inject(TYPES.RedisClient) private readonly redisClient: RedisClientType
+    @inject(TYPES.RedisClient) private readonly redisClient: Redis
   ) {}
 
   async set(key: string, value: string, ttl?: number): Promise<void> {
-     await this.redisClient.set(key, value, { EX: ttl });
+    if (ttl) {
+      await this.redisClient.set(key, value, "EX", ttl); 
+    } else {
+      await this.redisClient.set(key, value);
+    }
   }
 
   async get(key: string): Promise<string | null> {
