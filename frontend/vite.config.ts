9d84cc0e610ba14e609729a145a53aadee725777
AJMALAJU3 <ajmalaju3@users.noreply.github.com>
2025-09-17 09:42:33 +0530

    fix: vercel routing issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
@@ -14,4 +13,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist", 
+    emptyOutDir: true, 
+  }
 });
